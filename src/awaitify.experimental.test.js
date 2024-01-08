7a7b8b0111a18f26c48fcc1a53b1148906640dce
Mike Austin <mike_ekim@yahoo.com>
2024-01-08 11:56:22 -0800

    Add "add(2, 3)" test

--- src/awaitify.experimental.test.js
+++ src/awaitify.experimental.test.js
@@ -1,11 +1,37 @@
 const { asyncify } = require('./awaitify.js');
 
-test(`Promise.all([fetch('https://google.com')])`, () => {
+test(`const [a, b] = await Promise.all([fetch('https://google.com')])`, () => {
   const inputCode = `
-const urls = Promise.all([fetch('https://google.com')]);
+const [a, b] = await Promise.all([fetch('https://google.com'), fetch('https://apple.com')]);
   `;
 
   const expectedOutputCode = `???`;
+
+  const modifiedCode = asyncify(inputCode);
+
+  expect(modifiedCode).toBe(expectedOutputCode);
+});
+
+test(`add(2, 3)`, () => {
+  const inputCode = `
+function add(a, b) {
+  return a + b;
+}
+
+function random() {
+  return Math.random();
+}
+
+add(random(), random());
+  `;
+
+  const expectedOutputCode = `async function add(a, b) {
+    return await a + await b;
+  }  
+  async function random() {
+    return Math.random();
+  }
+  await add(random(), random());`;
 
   const modifiedCode = asyncify(inputCode);
 
